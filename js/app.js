dbebf292a52b21c6bd76d2768e069b0059c2d8aa
NoxThur <159153764+NoxThur@users.noreply.github.com>
2024-02-07 12:20:11 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let NumeroSecreto = 5;
+
+let chute = prompt("Digite um numero");
+
+if (chute == 5){
+    alert("VOCE ACERTOU O NUMERO!")
+} else if (chute > 5){
+    alert("O numero digitado é maior que o numero secreto")
+} else{
+    alert("O numero digitado é menor que o numero secreto")
+}
